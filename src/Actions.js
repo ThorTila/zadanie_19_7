ff2401049b32155a3e86ee6b78d01408de1fc512
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-11 12:24:29 +0200

    initial

---
+++ src/Actions.js
@@ -0,0 +1,43 @@
+import uuid from 'uuid';
+
+export const ADD_COMMENT = 'ADD_COMMENT',
+    REMOVE_COMMENT = 'REMOVE_COMMENT',
+    EDIT_COMMENT = 'EDIT_COMMENT',
+    THUMB_UP_COMMENT = 'THUMB_UP_COMMENT',
+    THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+export function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id: id
+    }
+}
+export function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id: id
+    }
+}
+export function thumbUp(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id: id,
+        thumb: 'up'
+    }
+}
+export function thumbDown(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: id,
+        thumb: 'down'
+    }
+}
+
+export function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    id: uuid.v4(),
+    text: text
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import {addComment} from './Actions';
+
+
+/* import { combineReducers } from 'redux'; */
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
