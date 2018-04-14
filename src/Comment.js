a7ed618b80daff5a20ef17adc83a36c865e9e1e7
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-14 09:51:25 +0200

    add autoselect text on inputs

--- src/Comment.js
+++ src/Comment.js
@@ -8,16 +8,19 @@ class Comment extends React.Component {
       value: this.props.text
     }
   }
+
   handleClick() {
     this.setState({
       isEdited: !this.state.isEdited
     })
   }
+
   handleChange(e) {
     this.setState({
       value: e.target.value
     })
   }
+
   handleSubmit(e) {
     e.preventDefault();
     let changedText = this.state.value;
@@ -27,29 +30,25 @@ class Comment extends React.Component {
       value: changedText
     });
   }
+  
   render() {
     return (
     <li>
+
         { this.state.isEdited === true ? 
           (<form onSubmit={(e) => this.handleSubmit(e)}>
-            <input autoFocus type="text" onChange={(e) => this.handleChange(e)} value={this.state.value} placeholder="Edit comment"/>
+            <input autoFocus onFocus={e => e.target.select()} type="text" onChange={(e) => this.handleChange(e)} value={this.state.value} placeholder="Edit comment"/>
             <button type="submit">Edit</button>
           </form>) : 
+
           (<p onClick={() => this.handleClick()}>{this.props.text} <span>votes: {this.props.votes}</span></p>) 
         }
+
         <button onClick={() => this.props.thumbUpComment(this.props.id)}>Thumb up</button>
         <button onClick={() => this.props.thumbDownComment(this.props.id)}>Thumb down</button>
         <button onClick={() => this.props.deleteComment(this.props.id)}>Delete</button>
       </li>
     )}
 }
-
-/* const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, deleteComment}) =>
-  <li>
-    {text} <span>votes: {votes}</span>
-    <button onClick={() => thumbUpComment(id)}>Thumb up</button>
-    <button onClick={() => thumbDownComment(id)}>Thumb down</button>
-    <button onClick={() => deleteComment(id)}>Delete</button>
-  </li>; */
 
 export default Comment;
